458dd4285bdf6d7d4f1f26514e4cc3d1fb5d02e3
azharfa99 <azharfaturohman29@gmail.com>
2025-01-24 01:38:56 +0700

    Comment out the logging configuration in settings.py for improved clarity and potential future use.

--- Platform/settings.py
+++ Platform/settings.py
@@ -324,24 +324,24 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     X_FRAME_OPTIONS = 'DENY'
 
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'handlers': {
-            'file': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': os.path.join(BASE_DIR, 'logs', 'app.log'),
-            },
-        },
-        'loggers': {
-            'django': {
-                'handlers': ['file'],
-                'level': 'DEBUG',
-                'propagate': True,
-            },
-        },
-    }
+    # LOGGING = {
+    #     'version': 1,
+    #     'disable_existing_loggers': False,
+    #     'handlers': {
+    #         'file': {
+    #             'level': 'DEBUG',
+    #             'class': 'logging.FileHandler',
+    #             'filename': os.path.join(BASE_DIR, 'logs', 'app.log'),
+    #         },
+    #     },
+    #     'loggers': {
+    #         'django': {
+    #             'handlers': ['file'],
+    #             'level': 'DEBUG',
+    #             'propagate': True,
+    #         },
+    #     },
+    # }
 
 
 
