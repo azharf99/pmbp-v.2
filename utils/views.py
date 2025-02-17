b490e3e36c984f02c96a4f61911e128f5249cd1a
azharfa99 <azharfaturohman29@gmail.com>
2025-02-18 00:03:26 +0700

    Enhance Kurasi search and pagination functionality
    - Modify search input placeholder and remove country search
    - Update per_page options with more flexible page size selections
    - Improve API query logic to combine results from multiple endpoints
    - Add additional search endpoint for organizer names
    - Implement result merging to prevent duplicate entries

--- utils/views.py
+++ utils/views.py
@@ -32,19 +32,44 @@ class CurrationListView(TemplateView):
         rate_kurasi = self.request.GET.get("rate_kurasi", "")
         
         if query or category or rate_kurasi or organizer_type:
-            url = f"https://simt.kemdikbud.go.id/api/v2/list-kurasi?page={page}&per_page={per_page}&organizer={query}&competition_name={query}&shorter_query={query}&category={category}&rate_kurasi={rate_kurasi}&country={query}&organizer_type={organizer_type}&start_date={start_date}&end_date={end_date}"
+            url = f"https://simt.kemdikbud.go.id/api/v2/list-kurasi?page={page}&per_page={per_page}&competition_name={query}&category={category}&rate_kurasi={rate_kurasi}&organizer_type={organizer_type}&start_date={start_date}&end_date={end_date}"
+            url_organizer = f"https://simt.kemdikbud.go.id/api/v2/list-kurasi?page={page}&per_page={per_page}&organizer={query}&category={category}&rate_kurasi={rate_kurasi}&organizer_type={organizer_type}&start_date={start_date}&end_date={end_date}"
         else:
             url = f"https://simt.kemdikbud.go.id/api/v2/list-kurasi?page={page}&per_page={per_page}"
     
-
+        
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
             "Accept": "application/json, text/javascript, */*; q=0.01",
             "Referer": "https://simt.kemdikbud.go.id/"
         }
 
-        response = requests.get(url, headers=headers)
-        res = response.json()
+        if query:
+            response1 = requests.get(url, headers=headers)
+            response2 = requests.get(url_organizer, headers=headers)
+            res = response1.json()
+            res1 = response2.json()
+
+            list1 = res['result']['list']
+            list2 = res1['result']['list']
+
+            length1 = len(list1)
+            length2 = len(list2)
+
+            if length1 > length2:
+                for data in list2:
+                    if not data in list1:
+                        list1.append(data)
+            else:
+                for data in list1:
+                    if not data in list1:
+                        list2.append(data)
+                res = res1
+        else:
+            response = requests.get(url, headers=headers)
+            res = response.json()
+        
+        print(res)
 
         context = super().get_context_data(**kwargs)
         total_page = res['result']['pagination']['total_page']
