5c54eb3e5ac5926568571af0e8a3c7db36a2697b
azharfa99 <azharfaturohman29@gmail.com>
2025-07-03 23:10:12 +0700

    Perbaiki pengaturan URL untuk debug toolbar dengan memindahkan penambahan path ke dalam blok kondisi, meningkatkan struktur dan keterbacaan file urls.py.

--- Platform/urls.py
+++ Platform/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -98,14 +97,14 @@ urlpatterns = [
     name="django.contrib.sitemaps.views.sitemap",
     ),
     path('service-worker.js', TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript')),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 
 if not settings.TESTING:
     urlpatterns = [
         *urlpatterns,
-    ] + debug_toolbar_urls()
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
