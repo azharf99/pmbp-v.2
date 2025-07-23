421597054a10811eca4d082ab58362b36bfbbbb8
azharfa99 <azharfaturohman29@gmail.com>
2025-07-24 04:25:47 +0700

    Tambahkan otentikasi dan izin pada tampilan notifikasi untuk meningkatkan keamanan akses pengguna terhadap fitur notifikasi.

--- notifications/views.py
+++ notifications/views.py
@@ -1,15 +1,19 @@
 # views.py
 from typing import Any
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from .models import Notification
 
-class NotificationIndexView(ListView):
+class NotificationIndexView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Notification
+    permission_required = 'notifications.view_notification'
+    
+    
 
-class NotificationDetailView(DetailView):
+class NotificationDetailView(LoginRequiredMixin, DetailView):
     model = Notification
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
@@ -21,24 +25,28 @@ class NotificationDetailView(DetailView):
             raise PermissionDenied
         return super().get(request, *args, **kwargs)
 
-class NotificationCreateView(CreateView):
+class NotificationCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Notification
     fields = '__all__'
     template_name = 'nilai/score_form.html'
     success_url = reverse_lazy('notification-list')
+    permission_required = 'notifications.add_notification'
 
-class NotificationQuickCreateView(CreateView):
+class NotificationQuickCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Notification
     fields = '__all__'
     template_name = 'nilai/score_form.html'
     success_url = reverse_lazy('notification-list')
+    permission_required = 'notifications.add_notification'
 
-class NotificationUpdateView(UpdateView):
+class NotificationUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Notification
     fields = '__all__'
     template_name = 'nilai/score_form.html'
     success_url = reverse_lazy('notification-list')
+    permission_required = 'notifications.change_notification'
     
-class NotificationDeleteView(DeleteView):
+class NotificationDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Notification
     success_url = reverse_lazy('notification-list')
+    permission_required = 'notifications.delete_notification'
