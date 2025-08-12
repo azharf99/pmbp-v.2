52e12a815ce51c2e8048917561f31a7897321a27
azharfa99 <azharfaturohman29@gmail.com>
2025-08-12 23:15:10 +0700

    Perbarui kelas ReportQuickCreateViewV3 dan ReportPutriQuickCreateViewV3 untuk mengaktifkan pengambilan nama kelas berdasarkan kategori "Putra" dan "Putri". Hapus kode yang tidak digunakan dan pastikan class_name diisi dengan data yang relevan.

--- reports/views.py
+++ reports/views.py
@@ -34,8 +34,7 @@ class ReportQuickCreateViewV3(QuickReportMixin):
     form_class = ReportFormV2
     template_name = 'reports/report_quick_form-v3.html'
     permission_required = 'reports.add_report'
-    # class_name = [name.short_class_name for name in Class.objects.filter(category="Putra")]
-    class_name = []
+    class_name = [name.short_class_name for name in Class.objects.filter(category="Putra")]
     queryset = Report.objects.select_related("schedule__schedule_course__course", "schedule__schedule_course__teacher", "schedule__schedule_time", "schedule__schedule_class", "schedule__teacher", "subtitute_teacher", "reporter")
 
 
@@ -46,8 +45,7 @@ class ReportPutriQuickCreateViewV3(QuickReportMixin):
     template_name = 'reports/report_quick_form-v3.html'
     permission_required = 'reports.add_report'
     type = "putri"
-    # class_name = [name.short_class_name for name in Class.objects.filter(category="Putri")]
-    class_name = []
+    class_name = [name.short_class_name for name in Class.objects.filter(category="Putri")]
 
     queryset = Report.objects.select_related("schedule__schedule_course__course", "schedule__schedule_course__teacher", "schedule__schedule_time", "schedule__schedule_class", "schedule__teacher", "subtitute_teacher", "reporter")
 
